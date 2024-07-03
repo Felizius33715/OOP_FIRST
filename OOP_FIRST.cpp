89750b1e3ee484307909d7461b3e531ca6ea4f6f
Felizius <Felizius@yandex.ru>
2024-07-03 20:56:04 +0700

    Добавьте файлы проекта.

---
+++ OOP_FIRST.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+class Figure {
+protected:
+    int sides;
+public:
+    Figure() : sides(0) {}
+    int getSides() const {
+        return sides;
+    }
+};
+
+class Triangle : public Figure {
+public:
+    Triangle() {
+        sides = 3;
+    }
+};
+
+class Quadrilateral : public Figure {
+public:
+    Quadrilateral() {
+        sides = 4;
+    }
+};
+
+int main() {
+    Figure unknownFigure;
+    Triangle triangle;
+    Quadrilateral quadrilateral;
+    std::cout << "Figure: " << unknownFigure.getSides() << std::endl;
+    std::cout << "Triangle: " << triangle.getSides() << std::endl;
+    std::cout << "Quadangle: " << quadrilateral.getSides() << std::endl;
+    return 0;
+}
